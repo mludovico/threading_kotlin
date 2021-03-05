cd7e0d6b4a5c0afb47b3c78c67bc9f7307e945da
Marcelo Ludovico de Paula <marcelo.paula@eptv.com.br>
2021-03-05 16:09:22 -0300

    Fix loadData return

--- app/src/main/java/br/com/mludovico/threading/AstrosRepository.kt
+++ app/src/main/java/br/com/mludovico/threading/AstrosRepository.kt
@@ -1,18 +1,19 @@
 package br.com.mludovico.threading
 
+import android.util.Log
 import com.google.gson.Gson
 import okhttp3.OkHttpClient
 import okhttp3.Request
 
 class AstrosRepository {
 
-    fun loadData(): List<AstroPeople>? {
+    fun loadData(): List<AstroPeople> {
         val client = OkHttpClient()
         val request = Request.Builder()
             .url("http://api.open-notify.org/astros.json")
             .build()
         val response = client.newCall(request).execute()
-        val result = parseJsonToResult(response.body?.toString())
+        val result = parseJsonToResult(response.body?.string())
         return result.people
     }
 
